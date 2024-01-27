8045e7f33e19b925f8408f98dba976da451908ea
Krish Tak <takrishtak2002@gmail.com>
2024-01-27 12:04:52 +0530

    Created variable of type string, boolean and created Functions

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
 const age: number = 22;
 console.log(age);
+
+const name: string = 'John Doe';
+console.log(name);
+
+const isAuthenticated: boolean = true;
+console.log(isAuthenticated);
+
+function addTwo(num: number) {
+	return num + 2;
+}
+
+const result = addTwo(5);
+console.log(result);
+
+export {};
